fc9f9f695b4ee5681f0637c6a61f40b9b0b0de4b
hung nguyen <nd.hung@me.com>
2024-03-25 00:20:09 +0100

    updated with coachs review

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import { HeaderImage } from "@/components/StyledComponents";
-import Image from "next/image";
 
 export default function HomePage() {
   return (
     <>
       <HeaderImage>
-      <h1>CAPSTONE PROJECT</h1>
-      <h3>Cats are fascinating creatures that have been revered throughout history for their grace, agility, and independence. They are known for their playful nature, their ability to hunt with precision, and their knack for finding the coziest spots to nap in. Cats come in a variety of breeds, each with its own unique characteristics and personalities. Whether they are sleek and elegant long-haired Persians or energetic and curious short-haired tabbies, cats bring joy and companionship to many households around the world. It's no wonder they have earned the title of being one of the most popular pets globally.</h3>
+        <h1>CAPSTONE PROJECT</h1>
+        <h3>
+          Cats are fascinating creatures that have been revered throughout
+          history for their grace, agility, and independence. They are known for
+          their playful nature, their ability to hunt with precision, and their
+          knack for finding the coziest spots to nap in. Cats come in a variety
+          of breeds, each with its own unique characteristics and personalities.
+          Whether they are sleek and elegant long-haired Persians or energetic
+          and curious short-haired tabbies, cats bring joy and companionship to
+          many households around the world. It&apos;s no wonder they have earned
+          the title of being one of the most popular pets globally.
+        </h3>
       </HeaderImage>
     </>
   );
 }
-      
